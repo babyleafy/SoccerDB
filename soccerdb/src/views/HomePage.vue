a286eb7a449e38a19fc77f6a6759e7bee15893da
gorgew <78799838+gorgew@users.noreply.github.com>
2023-04-20 10:37:39 -0400

    Add carousel with links

--- soccerdb/src/views/HomePage.vue
+++ soccerdb/src/views/HomePage.vue
@@ -1,32 +1,17 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <v-carousel cycle height="400" hide-delimiter-background show-arrows="hover">
+      <v-carousel-item v-for="(slide, i) in slides" :key="i" @click="onCarsouelClick(slides[i])">
+        <v-sheet :color="colors[i]" height="100%">
+          <div class="d-flex fill-height justify-center align-center" >
+            <div class="text-h2">
+              {{ slide }}
+            </div>
+          </div>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
@@ -35,24 +20,34 @@ export default {
   name: 'HomePage',
   props: {
     msg: String
-  }
+  },
+
+  data () {
+      return {
+        colors: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+        ],
+        slides: [
+          'Players',
+          'Clubs',
+          'Matches',
+          'Top'
+        ],
+        routes: [
+          'players',
+          'clubs',
+          'matches',
+          'top'
+        ],
+      }
+    },
+    methods: {
+      onCarsouelClick(i) {
+        this.$router.push(`/${i}`)
+      }
+    }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>

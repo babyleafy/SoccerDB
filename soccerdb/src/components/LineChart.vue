a4ce1021f678fbe793ec9fe3f33266d329f0e1a7
gorgew <78799838+gorgew@users.noreply.github.com>
2023-04-26 15:09:01 -0400

    Add chart components

---
+++ soccerdb/src/components/LineChart.vue
@@ -0,0 +1,56 @@
+<template>
+    <h1> LINECHART </h1>
+    <Line :data="data" :options="options" />
+</template>
+
+<script>
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 39, 10, 40, 39, 80, 40]
+    }
+  ]
+}
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: true
+}
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export default {
+    name: "LineChart",
+    components: {
+        Line
+    },
+    data() {
+        return {data, options}
+    }
+}
+</script>
